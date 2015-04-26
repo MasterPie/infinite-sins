e226ddc538b70e6360a35a0d6b9cd3b84250c773
MasterPie <VivekPai@live.com>
2015-04-26 01:33:56 +0000

    Webcam contrast settings

--- edison/extract_faces.py
+++ edison/extract_faces.py
@@ -16,7 +16,9 @@ from urllib import urlencode
 PADDING = 20
 
 video_capture = cv2.VideoCapture(0)
-video_capture.set(cv.CV_CAP_PROP_CONTRAST,0.07)
+video_capture.set(cv.CV_CAP_PROP_CONTRAST,0.1)
+video_capture.set(cv.CV_CAP_PROP_BRIGHTNESS, 0.08)
+video_capture.set(cv.CV_CAP_PROP_SATURATION, 0.02)
 ret, img = video_capture.read()
 video_capture.release()
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
